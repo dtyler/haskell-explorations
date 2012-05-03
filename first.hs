578dc3477eb212e5ae312f7114fc27885661ac4a
bame <dtyler@csh.rit.edu>
2012-05-02 21:25:08 -0400

    More messing around

--- first.hs
+++ first.hs
@@ -1,11 +1,16 @@
-name = "Dan"
-myAge = 23
-current_year = 2012
-
-birth_year cur age = cur - age
-
 triangles permeter = [ (a,b,c) | c <- [1..permeter],
                                  b <-[1..permeter],
                                  a <- [1..permeter],
                                  a + b + c <= permeter,
                                  a^2 + b^2 == c^2 ]
+
+percentError :: Float -> Float -> Float
+percentError test actual =  (max test actual - min test actual) / actual
+
+minRange :: Float -> Float -> Float
+minRange maxError actualValue = actualValue - (actualValue * maxError)
+maxRange :: Float -> Float -> Float
+maxRange maxError actualValue = actualValue + (actualValue * maxError)
+
+errorRange :: [a] -> Float -> Float
+errorRange maxError actualValue = [(minRange maxError actualValue)..(maxRange maxError actualValue)]
